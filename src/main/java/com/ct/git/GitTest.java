df480070604cdda343d8154def35767a96d9cb6a
Ajiang-del <lingjiang99999@gmail.com>
2022-01-29 10:01:45 +0800

    Push commit1

--- src/main/java/com/ct/git/GitTest.java
+++ src/main/java/com/ct/git/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("Hello Git!!!");
         System.out.println("Hello Git2!!!");
         System.out.println("Hello Git:Hot-fix");
+        System.out.println("Push test!!!");
     }
 }
+  
